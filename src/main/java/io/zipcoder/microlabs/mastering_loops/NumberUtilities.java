2250e217cc7fb9f80397abb9691b3b342879c37f
Emory <emory@Zipcoder-Emorys-MacBook-Pro.local>
2022-10-29 23:59:05 -0400

    checkpointing this file

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -1,36 +1,70 @@
 package io.zipcoder.microlabs.mastering_loops;
-
+import java.lang.Math;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String even = "";
+        for (int i = start; i < stop; i++){
+            if (i % 2 == 0) {
+                even = even + i;
+            }
+        }
+        return even;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String odd = "";
+        for (int i = start; i < stop; i++){
+            if (i % 2 != 0) {
+                odd = odd + i;
+            }
+        }
+        return odd;
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        String square = "";
+        for (int i = start; i < stop; i+=step){
+            square = square + (i * i);
+        }
+        return square;
     }
 
-    public static String getRange(int start) {
-        return null;
+    public static String getRange(int stop) {
+        String range = "";
+        for (int i = 0; i < stop; i++){
+            range = range + i;
+        }
+        return range;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String range = "";
+        for (int i = start; i < stop; i++){
+            range = range + i;
+        }
+        return range;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String range = "";
+        for (int i = start; i < stop; i+=step){
+            range = range + i;
+        }
+        return range;
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        String number = "";
+        for (int i = start; i < stop; i+=step ){
+            double power = Math.pow(i, exponent);
+            int powerint = (int) power;
+            number = number + powerint;
+        }
+        return number;
     }
 }
