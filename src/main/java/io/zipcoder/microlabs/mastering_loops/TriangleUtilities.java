8bd652d0d69279e5c0fb764fbaf98113f34f3375
Emory <emory@Zipcoder-Emorys-MacBook-Pro.local>
2022-10-30 23:32:49 -0400

    I can't get the tables or triangles to print correctly

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
@@ -3,18 +3,55 @@ package io.zipcoder.microlabs.mastering_loops;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+//        I can't figure out how to get this to print.
+//        I am able to get the logic to work.
+//        If I import this as a new class and run it, it will create a triangle.
+        String stars = "";
+        for (int i = 1; i <= numberOfRows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        return stars;
     }
 
     public static String getRow(int numberOfStars) {
-        return null;
+//        I can't figure out how to get this to print.
+//        I am able to get the logic to work.
+//        If I import this as a new class and run it, it will create a triangle.
+        String stars = "";
+        for (int i = 0; i < numberOfStars; i++) {
+            stars += "*";
+        }
+        return stars;
     }
 
-    public static String getSmallTriangle() {
-        return null;
+    public static String getSmallTriangle(int numberOfStars) {
+//        I can't figure out how to get this to print.
+//        I am able to get the logic to work.
+//        If I import this as a new class and run it, it will create a triangle.
+        String stars = "";
+        for (int i = 1; i <= numberOfStars; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        return stars;
     }
 
-    public static String getLargeTriangle() {
-        return null;
+    public static String getLargeTriangle(int numberOfStars) {
+//        I can't figure out how to get this to print.
+//        I am able to get the logic to work.
+//        If I import this as a new class and run it, it will create a triangle.
+        String stars = "";
+        for (int i = 1; i <= numberOfStars; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        return stars;
     }
 }
